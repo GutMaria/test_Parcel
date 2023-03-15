58aeb26e892ca7019d2063801a5b557f0bd6d0e7
GutMaria <gootmaria@gmail.com>
2023-03-15 13:24:28 +0200

    HW-08 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,35 @@
-console.log('Працює');
+// В консолі ERROR, мабуть плеєр блокує щось. СПИТАТИ!!!!!!!
 
 import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+// Спочатку підгружаємо час для відтворення з localStorage
+loadCurrentTime();
+player.on('timeupdate', throttle(saveCurrentTime, 1000) );
+    
+
+function saveCurrentTime(evt) {
+
+    const seconds = evt.seconds;
+    localStorage.setItem('videoplayer-current-time', seconds);
+    console.log(evt.seconds);
+    
+}
+
+
+function loadCurrentTime() {
+    try {
+        const secondForLoad = localStorage.getItem('videoplayer-current-time');
+        player.setCurrentTime(secondForLoad);
+        console.log(secondForLoad);
+        } catch (error) {
+            player.setCurrentTime(0);
+    }
+}
+
 
 
