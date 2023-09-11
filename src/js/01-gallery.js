05921770ffd2a9dd56667f87008ed0142a5c4a31
GutMaria <gootmaria@gmail.com>
2023-09-11 16:48:16 +0300

    add work files-js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const gallery = document.querySelector('.gallery');
+
+// 1. Створити розмітку 
+function createGalleryCard(arr) {
+    return arr.reduce(((acc, { preview, original, description }) => {
+      return  acc + `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`
+    }), '');
+}
+
+gallery.innerHTML = createGalleryCard(galleryItems);
+// 2. Підключаємо бібліотеку, в додаткових параметрах вказуємо затримку
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
 
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Спочатку підгружаємо час для відтворення з localStorage
+loadCurrentTime();
+player.on('timeupdate', throttle(saveCurrentTime, 1000) );
+    
+
+function saveCurrentTime(evt) {
+
+    const seconds = evt.seconds;
+    localStorage.setItem('videoplayer-current-time', seconds);
+    console.log(evt.seconds);
+    
+}
+
+
+function loadCurrentTime() {
+    try {
+        const secondForLoad = localStorage.getItem('videoplayer-current-time');
+        player.setCurrentTime(secondForLoad);
+        console.log(secondForLoad);
+        } catch (error) {
+            player.setCurrentTime(0);
+    }
+}
+
+
